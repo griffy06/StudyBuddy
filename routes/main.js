fa6ef8bd116a83356c34544e6ef937e41752f01a
Rohini Mishra <iit2018087@iiita.ac.in>
2020-06-11 04:17:59 +0530

    Added functionality- create post

--- routes/main.js
+++ routes/main.js
@@ -30,6 +30,68 @@ router.get('/course/:id/view', function (req,res,next) {
         });
 });
 
+router.get('/course/:id/create', function (req,res,next) {
+    // console.log('done');
+    //let course= Course.find({course_id:req.params.id});
+            Course.find({course_id:req.params.id},{},function(err1,course1){
+                console.log(course1);
+                if (err1) {
+                    console.log('No such entry');
+                    return;
+                }
+                else {
+                    Course.find({sem_id: course1[0].sem_id}, {}, function (err1, course) {
+                        console.log(course);
+                        if (err1) {
+                            console.log('No such entry');
+                            return;
+                        } else {
+                            res.render('create_post', {course: course, current_course: req.params.id});
+                        }
+                    });
+                }
+            });
+});
+
+router.post('/course/:id/create', function (req,res,next) {
+    // console.log('done');
+    //let course= Course.find({course_id:req.params.id});
+    let p= new Post();
+    p.no_of_dislikes=0;
+    p.no_of_likes=0;
+    p.author=req.body.author;
+    p.content=req.body.content;
+
+    Course.find({course_id:req.params.id},{},function(err1,course1){
+                console.log(course1);
+                if (err1) {
+                    console.log('No such entry');
+                    return;
+                }
+                else {
+                    Course.find({sem_id: course1[0].sem_id}, {}, function (err, course) {
+                        console.log(course);
+                        if (err) {
+                            console.log('No such entry');
+                            return;
+                        } else {
+                            p.course_id=req.params.id;
+                            p.sem_id=course1[0].sem_id;
+                            p.save(function(err2){
+                                if(err2){
+                                    console.log(err2);
+                                }
+                            else{
+                                res.render('courses', {course: course, sem: course[0].sem_id});
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+});
+
 router.get('/:id', function (req,res,next) {
     console.log(req.params.id);
     Course.find({sem_id:req.params.id},{}, function (err, course) {
